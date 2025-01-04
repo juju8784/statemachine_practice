71112c7ad4ecd4dfc3e1e6cca22d5b5a30ec404d
juju8784 <juliangarcia8784@gmail.com>
2025-01-03 20:32:11 -0600

    Creating the base classes for the statemachine

--- Assets/_Scripts/StateMachine/BaseState.cs
+++ Assets/_Scripts/StateMachine/BaseState.cs
@@ -1,6 +1,17 @@
+using System;
 using UnityEngine;
 
-public class BaseState
+
+
+
+public abstract class BaseState<EState> where EState : Enum
 {
-    
+    public abstract void EnterState();
+    public abstract void ExitState();
+    public abstract void UpdateState();
+    public abstract EState GetNextState();
+    public abstract void OnTriggerEnter();
+    public abstract void OnTriggerStay();
+    public abstract void OnTriggerExit();
 }
+
