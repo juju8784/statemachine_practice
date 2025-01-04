8852d61c578b00f4f214faaa6e29598ef2ca4171
juju8784 <juliangarcia8784@gmail.com>
2025-01-04 00:25:23 -0600

    Changing class names and adding in the base functionality
    I decided to change the class 'StateManager' to 'StateMachine' to avoid using manager in a class that will be used numerous times. Managers should be 1 time in the scene. The base functionality of the StateMachine is added, however it needs a few improvements

--- Assets/_Scripts/StateMachine/BaseState.cs
+++ Assets/_Scripts/StateMachine/BaseState.cs
@@ -6,12 +6,20 @@ using UnityEngine;
 
 public abstract class BaseState<EState> where EState : Enum
 {
+    public BaseState(EState key)
+    {
+        StateKey = key;
+    }
+
+    public EState StateKey { get; private set; }
+
     public abstract void EnterState();
     public abstract void ExitState();
     public abstract void UpdateState();
     public abstract EState GetNextState();
-    public abstract void OnTriggerEnter();
-    public abstract void OnTriggerStay();
-    public abstract void OnTriggerExit();
+    public abstract void OnTriggerEnter(Collider other);
+    public abstract void OnTriggerStay(Collider other);
+    public abstract void OnTriggerExit(Collider other);
+
 }
 
---
+++ Assets/_Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public abstract class StateMachine<EState> : MonoBehaviour where EState : Enum
+{
+    protected Dictionary<EState, BaseState<EState>> States = new Dictionary<EState, BaseState<EState>>();
+
+    protected BaseState<EState> CurrentState;
+
+    protected bool IsTransitioningState = false;
+
+    void Start() {
+        CurrentState.EnterState();
+    }
+
+    void Update() {
+        EState nextStateKey = CurrentState.GetNextState();
+
+        //To make sure we aren't transitioning states
+        if (!IsTransitioningState)
+        {
+            if (nextStateKey.Equals(CurrentState.StateKey))
+            {
+                CurrentState.UpdateState();
+
+            }
+            else
+            {
+                TransitionToState(nextStateKey);
+            }
+        }
+    }
+
+    public void TransitionToState(EState stateKey)
+    {
+        IsTransitioningState = true;
+        CurrentState.ExitState();
+        CurrentState = States[stateKey];
+        CurrentState.EnterState();
+        IsTransitioningState = false;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        CurrentState.OnTriggerEnter(other);
+    }
+
+    void OnTriggerStay(Collider other) {
+        CurrentState.OnTriggerStay(other);
+    }
+
+    void OnTriggerExit(Collider other) {
+        CurrentState.OnTriggerExit(other);
+    }
+}
